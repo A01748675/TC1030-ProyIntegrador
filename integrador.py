22a221b802cd71c2ae1869cab7b5f40b141d027c
avmejia-tec <avmejia@tec.mx>
2020-05-23 21:50:16 -0500

    Archivos iniciales

---
+++ integrador.py
@@ -0,0 +1,16 @@
+# Alumno :
+# Matricula :
+
+# importa la clase que sea el punto de partida de tu solucion
+
+
+class MiCatalogo:
+    def start(self):
+        pass
+        # aqui crea el obejto de tu clase princiapl y ejecuta
+        # tu aplicación
+
+
+if __name__ == "__main__":
+    streaming = MiCatalogo()
+    streaming.start()
